74412d72666d6e81fd3e13e5e602f136f47533fc
Sensatte <46267014+sensatte@users.noreply.github.com>
2021-01-26 22:55:21 +0100

    menu layout completed

--- menu/MenuLayout.py
+++ menu/MenuLayout.py
@@ -12,52 +12,37 @@ from kivy.app import App
 
 #import kv
 from kivy.lang import Builder
-
-"""
-class ButtonApp(App):  
-         
-    def build(self):  
-   
-        # create an image a button  
-        # Adding images normal.png image as button 
-        # decided its position and size  
-        btn = Button(text ="Push Me !", 
-                     color =(1, 0, .65, 1), 
-                     background_normal = 'images\icons\weather_min\01.png', 
-                     background_down ='images\icons\weather_min\02.png', 
-                     size_hint = (.3, .3), 
-                     pos_hint = {"x":0.35, "y":0.3} 
-                   )  
-      
-        return btn 
-
-root = ButtonApp()
-root.run()
-"""
+Builder.load_file('menu\menu.kv')
 
 class MenuLayout(FloatLayout):
     #import from external .kv
-    Builder.load_file('menu\menu.kv')
+    
     def __init__(self, **kwargs):
         super(MenuLayout, self).__init__(**kwargs)
 
         self.maxCols = 3
         self.maxRows = 3
 
-        self.selectedItem = (0,0)
-
+        self.selectedItem = (0,2)      
+        #TODO oscurecer fondo al abrir menu
+        #TODO que no escuche al teclado si no está menu
+        #TODO poner teclado en .py distinto
+                 
+        widgetList=['schedule', "news", "settings", "at", "clock", "schedule", "settings", "settings", "settings"]
+        ind=0
         self.itemsMatrix = {}
 
-        self.test1 = MenuOption(imageUri="images\\menu\\settings_pressed.png", maxCols=self.maxCols, maxRows=self.maxRows, posTuple=(0,0))
-        self.test2 = MenuOption(imageUri="images\\menu\\settings_pressed.png", maxCols=self.maxCols, maxRows=self.maxRows, posTuple=(1,1))
-        self.test3 = MenuOption(imageUri="images\\menu\\settings_pressed.png", maxCols=self.maxCols, maxRows=self.maxRows, posTuple=(2,2))
+        for i in range(self.maxCols):
+            for j in range(self.maxRows):
+                self.itemsMatrix[(i,j)]= MenuOption(imageUri="images\\menu\\" + widgetList[ind] + ".png", maxCols=self.maxCols, maxRows=self.maxRows, posTuple=(i,j))
+                ind+=1
+        
+        for widget in self.itemsMatrix.values():
+            self.add_widget(widget)
 
-        self.add_widget(self.test1)
-        self.add_widget(self.test2)
-        self.add_widget(self.test3)
+        self.updateButtons() #Para que empiece focuseado el widget
 
-
-        #self.itemsMatrix[self.test1.posTuple]=self.test1.
+        #TODO populatedmenu
 
 
 
@@ -67,99 +52,12 @@ class MenuLayout(FloatLayout):
 
         #Menu properties
         self.pos_hint={'center_y': 0, 'center_x': 0.5}
-        self.size_hint=0.2,0.2
+        self.size_hint=0.2,0.2 #valor inicial
         self.opacity=0
 
-
-        #Menu contents
-        #TODO Meterlos en un array o algo
-        #TODO Que se autocalculen las posiciones, o convertirlo en un BoxLayout o GridLayout or something iduno
-        #TODO Lógica para elegir uno de ellos y destacarlos, animandolo para que sea bigger que el resto o algo
-        #self.test1 = Image (source="images/icons/weather_medmin/01.png")
-        # self.test2 = Image (source="images/icons/weather_medmin/02.png")
-        # self.test3 = Image (source="images/icons/weather_medmin/03.png")
-        # self.test4 = Image (source="images/icons/weather_medmin/04.png")
-        # self.test5 = Image (source="images/icons/weather_medmin/05.png")
-        # self.test6 = Image (source="images/icons/weather_medmin/06.png")
-        # self.test7 = Image (source="images/icons/weather_medmin/07.png")
-        # self.test8 = Image (source="images/icons/weather_medmin/08.png")
-        # self.test9 = Image (source="images/icons/weather_medmin/08.png")
-
-
-        #self.test1.size_hint =(.1, .1)
-        # self.test2.size_hint =(.1, .1)
-        # self.test3.size_hint =(.1, .1)
-        # self.test4.size_hint =(.1, .1)
-        # self.test5.size_hint =(.1, .1)
-        # self.test6.size_hint =(.1, .1)
-        # self.test7.size_hint =(.1, .1)
-        # self.test8.size_hint =(.1, .1)
-        # self.test9.size_hint =(.1, .1)
-
-
-        #self.test1.pos_hint={'center_y': 0.15, 'center_x': 0.15}
-        # self.test2.pos_hint={'center_y': 0.15, 'center_x': 0.50}
-        # self.test3.pos_hint={'center_y': 0.15, 'center_x': 0.85}
-        # self.test4.pos_hint={'center_y': 0.50, 'center_x': 0.15}
-        # self.test5.pos_hint={'center_y': 0.50, 'center_x': 0.50}
-        # self.test6.pos_hint={'center_y': 0.50, 'center_x': 0.85}
-        # self.test7.pos_hint={'center_y': 0.85, 'center_x': 0.15}
-        # self.test8.pos_hint={'center_y': 0.85, 'center_x': 0.50}
-        # self.test9.pos_hint={'center_y': 0.85, 'center_x': 0.85}
-
-        #self.add_widget(self.test1)
-        # self.add_widget(self.test2)
-        # self.add_widget(self.test3)
-        # self.add_widget(self.test4)
-        # self.add_widget(self.test5)
-        # self.add_widget(self.test6)
-        # self.add_widget(self.test7)
-        # self.add_widget(self.test8)
-        # self.add_widget(self.test9)
-
-    def moveUp(self):
-        if self.selectedItem[1] < self.maxRows - 1:
-            self.selectedItem[1] += 1
-        else:
-            self.selectedItem[1] = 0
-        self.updateButtons()
-
-    def moveDown(self):
-        if self.selectedItem[1] > 0:
-            self.selectedItem[1] -= 1
-        else:
-            self.selectedItem = self.maxRows -1
-        self.updateButtons()
-
-    def moveRight(self):
-        if self.selectedItem[0] < self.maxCols - 1:
-            self.selectedItem[0] += 1
-        else:
-            self.selectedItem[0] = 0
-        self.updateButtons()
-
-    def moveLeft(self):
-        if self.selectedItem[0] > 0:
-            self.selectedItem[0] -= 1
-        else:
-            self.selectedItem[0] = self.maxCols -1
-        self.updateButtons()
-
-    def openSelected(self):
-        #TODO
-        print(self.selectedItem)
-
-    def updateButtons(self):
-        for x in range(0, self.maxCols-1):
-            for y in range(0, self.maxRows-1):
-                widget = self.itemsMatrix[(x,y)]
-                if ((x,y) == self.selectedItem):
-                    #TODO setear a 1
-                    widget
-                else:
-                    #TODO setear a 0
-                    pass                
         
+    # animations
+
     def fadeIn(self):
         anim = Animation(opacity=1, duration=.5)
         anim &= Animation(pos_hint={'center_y': 0.5, 'center_x': 0.5}, duration=.5)
@@ -172,8 +70,23 @@ class MenuLayout(FloatLayout):
         anim &= Animation(size_hint=(0.2,0.2), duration=.5)
         anim.start(self)
 
+    def focusOption(self, widget):
+        anim = Animation(opacity=1,size_hint=(0.28,0.28), duration=.1)
+        anim.start(widget)
+
     
-    #
+    def unfocusOption(self, widget):
+        anim = Animation(opacity=1,size_hint=(0.25,0.25), duration=.1)
+        anim.start(widget)
+
+    def pressedOption(self, widget):
+        anim = Animation(size_hint=(0.2,0.2), duration=.1)
+        anim += Animation(opacity=0,size_hint=(0.3,0.3), duration=.2)
+        anim.start(widget)
+
+
+    
+    # keyboard buttons
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -194,32 +107,75 @@ class MenuLayout(FloatLayout):
             self.moveUp()
         if keycode[1] == 's':
             self.moveDown()
-        if keycode[1] == 'space':
+        if keycode[1] == 'enter':
             self.openSelected()
 
         return True
 
-class MenuOption(Image):
 
-    
+    def moveUp(self):
+        if self.selectedItem[1] < self.maxRows - 1:
+            self.selectedItem = (self.selectedItem[0],self.selectedItem[1]+1)
+        else:
+            self.selectedItem = (self.selectedItem[0],0)
+        self.updateButtons()
+
+
+    def moveDown(self):
+        if self.selectedItem[1] > 0:
+            self.selectedItem = (self.selectedItem[0],self.selectedItem[1]-1)
+        else:
+            self.selectedItem = (self.selectedItem[0],self.maxRows -1)
+        self.updateButtons()
+
+
+    def moveRight(self):
+        if self.selectedItem[0] < self.maxCols - 1:
+            self.selectedItem = (self.selectedItem[0]+1,self.selectedItem[1])
+        else:
+            self.selectedItem = (0,self.selectedItem[1])
+        self.updateButtons()
+
+
+    def moveLeft(self):
+        if self.selectedItem[0] > 0:
+            self.selectedItem = (self.selectedItem[0]-1,self.selectedItem[1])
+        else:
+            self.selectedItem = (self.maxCols -1,self.selectedItem[1])
+        self.updateButtons()
+        
+
+    def openSelected(self):
+        #TODO abrir menu del widget                    
+        # #llama al método de la animación con el widget de selected item
+        self.pressedOption(self.itemsMatrix[(self.selectedItem[0],self.selectedItem[1])])         
+        print(self.selectedItem)
+        print(self.itemsMatrix[(self.selectedItem[0],self.selectedItem[1])])        
+
+
+    def updateButtons(self):
+        for col in range(0, self.maxCols):
+            for row in range(0, self.maxRows):
+                widget = self.itemsMatrix[(col,row)]
+                if ((col,row) == self.selectedItem):                    
+                    self.focusOption(widget)
+                else:
+                    self.unfocusOption(widget)             
+
+
+class MenuOption(Image):    
 
     def __init__(self, posTuple, maxCols, maxRows, imageUri):
         super(MenuOption, self).__init__()
-        spotsX = 2*maxCols+1
-        spotsY = 2*maxRows+1
-        paddingX = 1/spotsX
-        paddingY = 1/spotsY
 
-        #TODO
-        self.size_hint = 0.2,0.2
+        #Posiciones widgets
+        self.size_hint = 0.25,0.25
 
-       
-
-        self.pos_hint = {'center_x': paddingX*(1+posTuple[0]+1), 'center_y':paddingY*(1+posTuple[1]+1)}
-        
-        print ("\n\n\n")
-        print (self.pos_hint)
-        print ("\n\n\n")
+        self.focus=True
+        c = 1/maxCols
+        r = 1/maxRows
+    
+        self.pos_hint = {'center_x': c * (posTuple[0])+c/2, 'center_y': r * (posTuple[1])+r/2}
         
         self.source=imageUri
         self.posTuple = posTuple
